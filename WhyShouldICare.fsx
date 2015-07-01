b5949f63d85fcf293eaa49dfd7e21dd6f18c94b3
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2015-07-01 13:09:33 +0200

    add why should I care

---
+++ WhyShouldICare.fsx
@@ -0,0 +1,36 @@
+﻿(*
+I don't use F#/C#/Haskell/Erlang, why should I care?
+
+AUTOSAR at VOLVO Example, C-Code tested with Erlang: 
+https://vimeo.com/68331689
+
+---------------------------------
+Many other languages have ports:
+
+Re-implementations of QuickCheck exist for (according to Wikipedia)
+C,[2][3] 
+C++,[4][5][6] 
+Chicken Scheme,[7] 
+Clojure,[8][9][10] 
+Common Lisp,[11] 
+D,[12] 
+Elm, [13] 
+Factor,[15] 
+Io,[16] 
+Java,[17][18][19] 
+JavaScript,[20] 
+Node.js,[21] 
+Objective-C,[22] 
+OCaml,[23] 
+Perl,[24] 
+Prolog,[25][26] 
+Python,[27] 
+R,[28] 
+Ruby,[29] 
+Rust,[30] 
+Scala,[31] 
+Scheme,[32] 
+Smalltalk,[33] 
+Standard ML[34] 
+Swift.[35]
+*)
